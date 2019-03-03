86534d2979f83a86bd0fdf39532ab9b0d569cd1f
Victor Luzgin <luzgin.st@gmail.com>
2019-03-03 20:09:44 +0300

    Add logging and typing; refactor

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
+import logging
 import os
+from http import HTTPStatus
+from typing import Optional
 
 import requests
+from requests import RequestException
 
 API_URL = 'https://free.currencyconverterapi.com/api/v6/convert'
 API_KEY = os.environ['CURRENCY_CONVERTER_API_KEY']
@@ -11,8 +15,17 @@ REQUEST_PAYLOAD = {
 }
 
 
-def convert_value(value):
-    r = requests.get(url=API_URL, params=REQUEST_PAYLOAD)
+def get_current_exchange_rate() -> Optional[float]:
+    try:
+        response = requests.get(url=API_URL, params=REQUEST_PAYLOAD)
+    except RequestException:
+        logging.error('An exception occurred while handling a request.')
+        return
 
-    exchange_rate = r.json()['USD_RUB']
-    return exchange_rate * value
+    if response.status_code != HTTPStatus.OK:
+        logging.warning('Response status code not success')
+        return
+
+    data = response.json()
+    if data:
+        return data.get('USD_RUB')
--- converter.py
+++ converter.py
@@ -1,9 +1,38 @@
+import logging
 import sys
+from typing import Optional
 
-from client import convert_value
+from client import get_current_exchange_rate
 
-if __name__ == "__main__":
-    value = int(sys.argv[1])
+logging.basicConfig(
+    level=logging.INFO,
+    filename='app.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 
-    new_value = convert_value(value)
-    print(f'{value} USD = {new_value} RUB')
+
+def convert_value(dollars_count: float) -> Optional[float]:
+    exchange_rate = get_current_exchange_rate()
+    if exchange_rate is None:
+        return None
+    return exchange_rate * dollars_count
+
+
+def main():
+    try:
+        value = float(sys.argv[1])
+    except (ValueError, IndexError):
+        logging.warning('Incorrect input')
+        print('Input value is incorrect')
+    else:
+        new_value = convert_value(value)
+        if new_value is not None:
+            logging.info('Success')
+            print(f'{value} USD = {new_value} RUB')
+        else:
+            logging.warning('Exchange rate not received')
+            print('Current exchange rate not received')
+
+
+if __name__ == '__main__':
+    main()
